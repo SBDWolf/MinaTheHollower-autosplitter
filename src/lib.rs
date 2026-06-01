c852153f9f1a88839b4dde7e2c8109ae3d260b3e
Rynon <rynohn0147@gmail.com>
2026-06-01 19:22:26 +0200

    first pass

--- src/lib.rs
+++ src/lib.rs
@@ -14,16 +14,18 @@ async fn main() {
     // TODO: Set up some general state and settings.
     let mut settings = Settings::register();
 
-    asr::print_message("Hello, World!");
+    asr::print_message("Setup done. Waiting for Process.");
 
     loop {
         let process = Process::wait_attach("MinaTheHollower").await;
         process
             .until_closes(async {
+                asr::print_message("Process found.");
                 // TODO: Load some initial information from the process.
+                asr::print_message("Starting Loop.");
                 loop {
                     settings.update();
-
+                    
                     // TODO: Do something on every tick.
                     next_tick().await;
                 }
